dadb4d391c8e303afa8f00e3f0729bca68c91ebc
asim093 <asimusman8899@gmail.com>
2024-12-26 07:40:29 -0800

    updated

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "../backend/routes/auth.routes.js";
 import messageRoutes from "../backend/routes/message.routes.js";
@@ -9,10 +10,19 @@ import userRoutes from "../backend/routes/user.routes.js";
 
 import connectToMongoDB from "../backend/db/connectToMongoDB.js";
 import { app, server } from "../backend/socket/socket.js"; 
+
 dotenv.config();
 
-const __dirname = path.resolve();
+const __dirname = path.resolve(); 
 const PORT = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: "https://resonant-travesseiro-c14c6b.netlify.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"],  
+  credentials: true,  
+};
+
+app.use(cors(corsOptions));  
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,12 +30,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 server.listen(PORT, () => {
   connectToMongoDB();
