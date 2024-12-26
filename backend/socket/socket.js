862ef23414407aac94c21652eed3ce91a0d03f0b
asim093 <asimusman8899@gmail.com>
2024-12-26 07:28:29 -0800

    server.js in root

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,29 +1,48 @@
+import { Server } from "socket.io";
+import express from "express";
+import { createServer } from "http"; 
+
+const app = express();
+const server = createServer(app);  
+const io = new Server(server, {
+  // Socket.io options (optional)
+  cors: {
+    origin: "*",  
+    methods: ["GET", "POST"],
+  },
+});
+
+const userSocketMap = {};  
+
 io.on("connection", (socket) => {
-	console.log("a user connected", socket.id);
-  
-	const userId = socket.handshake.query.userId;
-	if (userId !== "undefined") {
-	  userSocketMap[userId] = socket.id;
-	}
-  
-	// Notify all clients of online users
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
-  
-	// Handle sending messages
-	socket.on("sendMessage", ({ senderId, receiverId, message }) => {
-	  const receiverSocketId = getReceiverSocketId(receiverId);
-	  if (receiverSocketId) {
-		io.to(receiverSocketId).emit("receiveMessage", {
-		  senderId,
-		  message,
-		});
-	  }
-	});
-  
-	socket.on("disconnect", () => {
-	  console.log("user disconnected", socket.id);
-	  delete userSocketMap[userId];
-	  io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+  console.log("a user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId !== "undefined") {
+    userSocketMap[userId] = socket.id;
+  }
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("sendMessage", ({ senderId, receiverId, message }) => {
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("receiveMessage", {
+        senderId,
+        message,
+      });
+    }
   });
-  
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
+export { app, server, io, userSocketMap };
